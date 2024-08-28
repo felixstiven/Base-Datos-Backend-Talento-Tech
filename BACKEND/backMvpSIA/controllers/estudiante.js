687dbc1349d4ef950e01fa795a1ebf8f2514d218
felixstiven <felixstiven12@gmail.com>
2024-08-28 07:05:25 -0500

    actualizando estudiante

--- BACKEND/backMvpSIA/controllers/estudiante.js
+++ BACKEND/backMvpSIA/controllers/estudiante.js
@@ -22,7 +22,33 @@ const controllerEstudiantes = {
         } catch (error){
             return res.status(500).send(error.message)
         }
+    },
+    getEstudiantePorId : async (req, res) => {
+        try{
+            const {id} = req.params
+            const estudiante = await Estudiante.findById(id)
+            res.json(estudiante).status(200)
+
+        } catch (error){
+            return res.status(500).send(error.message)
+        }
+    },
+    upDateEstudiantesPorId : async (req, res) =>{
+        try{
+            const {id} = req.params
+            await Estudiante.findByIdAndUpdate(id,{
+                nombre : req.body.nombre,
+                materias : req.body.materias,
+                asistencia : req.body.asistencia,
+                tareas : req.body.tareas
+            })
+            res.status(200).send("Estudinate actualizado");
+        } catch (error) {
+            return res.status(500).send(error.message)
+        }
+
     }
+
 }
 
 module.exports = controllerEstudiantes
--- BACKEND/backMvpSIA/routes/estudiante.js
+++ BACKEND/backMvpSIA/routes/estudiante.js
@@ -4,10 +4,10 @@ const controllerEstudiantes = require("../controllers/estudiante")
 const router = express.Router();
 
 // Tenemos que construir las rutas del CRUD
-
 router.get("/", controllerEstudiantes.getEstudiantes);
+router.get("/:id", controllerEstudiantes.getEstudiantePorId);
 router.post("/", controllerEstudiantes.create); //("crear estudiante")
-router.patch("/",()=>console.log("editar estudiante"));
+router.put("/:id", controllerEstudiantes.upDateEstudiantesPorId);
 router.delete("/",()=>console.log("eliminar estudiante"));
 
 module.exports = router
